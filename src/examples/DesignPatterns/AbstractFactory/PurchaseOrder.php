a761ec6223f3b2d0326039a44425eaf9dbab1188
Linnzh <reg.lynnzh@gmail.com>
2021-10-13 10:34:31 +0800

    fix #1: 抽象工厂模式（Abstract Factory） (#2)

---
+++ src/examples/DesignPatterns/AbstractFactory/PurchaseOrder.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Linnzh\Utils\examples\DesignPatterns\AbstractFactory;
+
+
+class PurchaseOrder implements OrderInterface
+{
+    protected \DateTimeInterface $createAt;
+
+    public function __construct(protected array $products = [])
+    {
+        $this->createAt = new \DateTimeImmutable();
+    }
+
+    public function calculateOrderAmount()
+    {
+        // Implement calculateOrderAmount() method.
+        return array_sum(array_column($this->products, 'amount'));
+    }
+}

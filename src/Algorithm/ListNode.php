c92d377b1b065d4c7317ee3c12e75097f49030d7
Linnzh <reg.lynnzh@gmail.com>
2022-03-03 13:29:44 +0800

    pref: 单链表前后序遍历

--- src/Algorithm/ListNode.php
+++ src/Algorithm/ListNode.php
@@ -15,13 +15,54 @@ class ListNode
     public mixed $value;
 
     /**
-     * @var TreeNode|null 指向下一节点的指针
+     * @var ListNode|null 指向下一节点的指针
      */
-    public ?TreeNode $next;
+    public ?ListNode $next;
 
     public function __construct(mixed $value)
     {
         $this->value = $value;
         $this->next = null;
+    }
+
+    /**
+     * 迭代遍历 p.value
+     *
+     * @param ListNode $head
+     * @return array
+     */
+    public function iterateTraverse(ListNode $head): array
+    {
+        $result = [];
+        for ($p = $head; $p !== null; $p = $p->next) {
+            $result[] = $p->value;
+        }
+        return $result;
+    }
+
+    /**
+     * 前序遍历
+     * @param ListNode $head
+     * @param array $result
+     * @return array
+     */
+    public function preorderTraverse(ListNode $head, array &$result = []): array
+    {
+        $result[] = $head->value;
+        $head->next && $this->preorderTraverse($head->next, $result);
+        return $result;
+    }
+
+    /**
+     * 后序遍历
+     * @param ListNode $head
+     * @param array $result
+     * @return array
+     */
+    public function postorderTraverse(ListNode $head, array &$result = []): array
+    {
+        $head->next && $this->postorderTraverse($head->next, $result);
+        $result[] = $head->value;
+        return $result;
     }
 }
---
+++ tests/Algorithm/ListNodeTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Linnzh\Utils\Test\Algorithm;
+
+use Linnzh\Utils\Algorithm\ListNode;
+use PHPUnit\Framework\TestCase;
+
+class ListNodeTest extends TestCase
+{
+
+    public function testIterateTraverse(): void
+    {
+        $node1 = new ListNode(1);
+        $node2 = new ListNode(3);
+        $node3 = new ListNode(5);
+
+        $node2->next = $node3;
+        $node1->next = $node2;
+
+        $result = $node1->iterateTraverse($node1);
+        $expected = [1, 3, 5];
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testPreorderTraverse(): void
+    {
+        $node1 = new ListNode(1);
+        $node2 = new ListNode(3);
+        $node3 = new ListNode(5);
+
+        $node2->next = $node3;
+        $node1->next = $node2;
+
+        $result = $node1->preorderTraverse($node1);
+        $expected = [1, 3, 5];
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testPostorderTraverse(): void
+    {
+        $node1 = new ListNode(1);
+        $node2 = new ListNode(3);
+        $node3 = new ListNode(5);
+
+        $node2->next = $node3;
+        $node1->next = $node2;
+
+        $result = $node1->postorderTraverse($node1);
+        $expected = [5, 3, 1];
+        $this->assertEquals($expected, $result);
+    }
+}

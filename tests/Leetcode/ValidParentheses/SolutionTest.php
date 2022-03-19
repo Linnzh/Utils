b31fda7a23ebef86dbb03c98bed7faf1c40fdeee
Linnzh <reg.lynnzh@gmail.com>
2022-03-20 04:11:16 +0800

    feat: leetcode-有效的括号

---
+++ tests/Leetcode/ValidParentheses/SolutionTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Linnzh\Utils\Test\Leetcode\ValidParentheses;
+
+use Linnzh\Utils\Leetcode\ValidParentheses\Solution;
+use PHPUnit\Framework\TestCase;
+
+class SolutionTest extends TestCase
+{
+
+    public function testIsValid(): void
+    {
+        $this->assertEquals(
+            true,
+            (new Solution())->isValid('()')
+        );
+
+        $this->assertEquals(
+            false,
+            (new Solution())->isValid('){')
+        );
+
+        $this->assertEquals(
+            true,
+            (new Solution())->isValid('()[]{}')
+        );
+
+        $this->assertEquals(
+            false,
+            (new Solution())->isValid('([)]')
+        );
+
+        $this->assertEquals(
+            false,
+            (new Solution())->isValid('()([)]')
+        );
+
+        $this->assertEquals(
+            true,
+            (new Solution())->isValid('{[]}')
+        );
+
+        $this->assertEquals(
+            true,
+            (new Solution())->isValid('(([]){})')
+        );
+
+        $this->assertEquals(
+            false,
+            (new Solution())->isValid('[({])}')
+        );
+
+//        $this->assertEquals(
+//            true,
+//            (new Solution())->isValid('[({])}')
+//        );
+    }
+}

a00dc27ea593138c2d972cb45655b2c3fd01301c
Linnzh <reg.lynnzh@gmail.com>
2022-03-26 15:23:43 +0800

    feat: 设计模式-责任链

---
+++ src/ChainOfResponsibility/RoleCheckMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Linnzh\Utils\ChainOfResponsibility;
+
+
+/**
+ * This Concrete Middleware checks whether a user associated with the request
+ * has sufficient permissions.
+ */
+class RoleCheckMiddleware extends Middleware
+{
+    public function check(string $email, string $password): bool
+    {
+        if ($email === "admin@example.com") {
+            echo "RoleCheckMiddleware: Hello, 管理员！\n";
+
+            return true;
+        }
+        echo "RoleCheckMiddleware: Hello, 普通用户！\n";
+
+        return parent::check($email, $password);
+    }
+}
---
+++ tests/ChainOfResponsibility/MiddlewareTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Linnzh\Utils\Test\ChainOfResponsibility;
+
+use Linnzh\Utils\ChainOfResponsibility\Middleware;
+use Linnzh\Utils\ChainOfResponsibility\RoleCheckMiddleware;
+use Linnzh\Utils\ChainOfResponsibility\Server;
+use Linnzh\Utils\ChainOfResponsibility\ThrottlingMiddleware;
+use Linnzh\Utils\ChainOfResponsibility\UserExistsMiddleware;
+use PHPUnit\Framework\TestCase;
+
+class MiddlewareTest extends TestCase
+{
+    public function testExample(): void
+    {
+        $server = new Server();
+        $server->register('admin@example.com', 'admin_pass');
+        $server->register('user@example.com', 'user_pass');
+
+        $middleware = new ThrottlingMiddleware(2);
+        $middleware
+            ->linkWith(new UserExistsMiddleware($server))
+            ->linkWith(new RoleCheckMiddleware());
+
+        $server->setMiddleware($middleware);
+
+
+        $success = $server->logIn('123', '123_pass');
+        $this->assertFalse($success);
+
+        $success = $server->logIn('1234', '1234_pass');
+        $this->assertFalse($success);
+
+        $server->logIn('user@example.com', 'user_pass');
+
+        $success = $server->logIn('admin@example.com', 'admin_pass');
+        $this->assertTrue($success);
+    }
+}

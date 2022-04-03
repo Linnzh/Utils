610f1d1fc71f24d138f260fcb5d0129cede32439
Linnzh <reg.lynnzh@gmail.com>
2022-04-03 23:09:12 +0800

    feat: leetcode-删除有序数组中的重复项

---
+++ tests/Leetcode/RemoveDuplicatesFromSortedArray/SolutionTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Linnzh\Utils\Test\Leetcode\RemoveDuplicatesFromSortedArray;
+
+use Linnzh\Utils\Leetcode\RemoveDuplicatesFromSortedArray\Solution;
+use PHPUnit\Framework\TestCase;
+
+class SolutionTest extends TestCase
+{
+    public function testExample(): void
+    {
+        $nums = [1, 1, 2];
+        $count = (new Solution())->removeDuplicates($nums);
+        $this->assertEquals([1, 2], $nums);
+        $this->assertEquals(2, $count);
+    }
+}

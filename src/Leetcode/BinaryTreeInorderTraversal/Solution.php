fbfcd08ca037d2e8f3b7cbb5d66104119cef279a
Linnzh <reg.lynnzh@gmail.com>
2022-04-04 04:30:56 +0800

    feat: leetcode-二叉树的中序遍历

---
+++ src/Leetcode/BinaryTreeInorderTraversal/Solution.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace Linnzh\Utils\Leetcode\BinaryTreeInorderTraversal;
+
+
+use Linnzh\Utils\Algorithm\TreeNode;
+
+
+/**
+ * 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+ *
+ * 示例 1：
+ *
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,3,2]
+ *
+ * 示例 2：
+ *
+ * 输入：root = []
+ * 输出：[]
+ *
+ * 示例 3：
+ *
+ * 输入：root = [1]
+ * 输出：[1]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-inorder-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class Solution
+{
+    /**
+     * 递归算法
+     * @TODO 迭代算法
+     *
+     * @param TreeNode $root
+     * @param array $result
+     * @return Integer[]
+     */
+    public function inorderTraversal(TreeNode $root, array &$result = []): array
+    {
+        $root->left && $this->inorderTraversal($root->left, $result);
+
+        $result[] = $root->value;
+
+        $root->right && $this->inorderTraversal($root->right, $result);
+
+        return $result;
+    }
+}
---
+++ tests/Leetcode/BinaryTreeInorderTraversal/SolutionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Linnzh\Utils\Test\Leetcode\BinaryTreeInorderTraversal;
+
+use Linnzh\Utils\Algorithm\TreeNode;
+use Linnzh\Utils\Leetcode\BinaryTreeInorderTraversal\Solution;
+use PHPUnit\Framework\TestCase;
+
+class SolutionTest extends TestCase
+{
+    public function testInorderTraversal(): void
+    {
+        $node1 = new TreeNode(1);
+        $node2 = new TreeNode(2);
+        $node3 = new TreeNode(3);
+        $node2->left = $node3;
+        $node1->left = null;
+        $node1->right = $node2;
+        $this->assertEquals([1,3,2], (new Solution())->inorderTraversal($node1));
+    }
+}

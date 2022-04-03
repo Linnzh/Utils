bf71d4461b3683d48bd1ac96f8c3d4fae2d1374a
Linnzh <reg.lynnzh@gmail.com>
2022-04-03 23:32:42 +0800

    feat: leetcode-实现 strStr()

---
+++ tests/Leetcode/ImplementStrStr/SolutionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Linnzh\Utils\Test\Leetcode\ImplementStrStr;
+
+use Linnzh\Utils\Leetcode\ImplementStrStr\Solution;
+use PHPUnit\Framework\TestCase;
+
+class SolutionTest extends TestCase
+{
+
+    public function testStrStr(): void
+    {
+        $this->assertEquals(0, (new Solution())->strStr('a','a'));
+        $this->assertEquals(0, (new Solution())->strStr('a',''));
+        $this->assertEquals(-1, (new Solution())->strStr('abc','d'));
+        $this->assertEquals(1, (new Solution())->strStr('abc','b'));
+    }
+}

e6c989a56c489ea22e25869d394b6abc6899d143
Linnzh <reg.lynnzh@gmail.com>
2023-03-07 22:53:52 +0800

    leetcode: 最长公共前缀

--- src/Functions.php
+++ src/Functions.php
@@ -25,3 +25,34 @@ function isPalindrome(int $x): bool {
 
     return $reversed == $x;
 }
+
+/**
+ * 最长公共前缀
+ *
+ * @param array|string[] $strArr
+ *
+ * @return string
+ */
+function longestCommonPrefix(array $strArr): string {
+    if (empty($strArr)) {
+        return '';
+    }
+
+    $count = count($strArr);
+    $length = strlen($strArr[0]);
+
+    for ($col = 0;$col < $length; $col++) {
+        for ($row = 1;$row < $count; $row++) {
+            $current = $strArr[$row];
+            $prev = $strArr[$row - 1];
+            if ($col >= strlen($current)
+                || $col >= strlen($prev)
+                || $current[$col] != $prev[$col]
+            ) {
+                return substr($strArr[$row], 0, $col);
+            }
+        }
+    }
+
+    return $strArr[0];
+}

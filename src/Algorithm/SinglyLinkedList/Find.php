932f1eac8f6c31c0611f0e71cad70004d5323630
Linnzh <reg.lynnzh@gmail.com>
2023-04-01 00:17:05 +0800

    快慢指针：判断链表是否包含环

--- src/Algorithm/SinglyLinkedList/Find.php
+++ src/Algorithm/SinglyLinkedList/Find.php
@@ -27,4 +27,32 @@ class Find
         }
         return $slow;
     }
+
+    /**
+     * 判断链表是否包含环
+     *
+     * 快慢指针：
+     * 每当慢指针 slow 前进一步，快指针 fast 就前进两步。
+     * 如果 fast 最终遇到空指针，说明链表中没有环；
+     * 如果 fast 最终和 slow 相遇，那肯定是 fast 超过了 slow 一圈，说明链表中含有环。
+     *
+     * @param ListNode $head
+     *
+     * @return bool
+     */
+    public function hasCycle(ListNode $head)
+    {
+        $slow = $head;
+        $fast = $head;
+
+        while ($fast && $fast->next) {
+            $slow = $slow->next;
+            $fast = $fast->next->next;
+
+            if ($slow === $fast) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
